699e05907d46df55fe38376708048cf311bbfaf8
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-07-25 12:26:23 +0530

    Time: 0 ms (100%), Space: 48.3 MB (31.89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,58 +1,57 @@
 class Solution {
-     public int findRightMost(int[] nums, int target, int n) {
-
+    public int findLeftMost(int[] nums, int target) {
         int l = 0;
-        int r = n - 1;
-        int left_most = -1;
+        int r = nums.length - 1;
+        int ans = -1;
+
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                left_most = mid;
-                l = mid +1;// left mae aur dekho 
-
-                //ye answer ho skta h par yahi answer nhi h 
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-
-            } else {
+            if (nums[mid] == target)
+            //ye answer ho skta h par yahi hoga we r not sure 
+            // so we travel to further left of the array 
+            {
+                ans = mid;
                 r = mid - 1;
 
-            }
-        }
-        return left_most;
-        
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else
+                r = mid - 1;
 
+        }
+        return ans;
     }
-    public int findLeftMost(int[] nums, int target, int n) {
-
+  public int findRightMost(int[] nums, int target) {
         int l = 0;
-        int r = n - 1;
-        int right_most = -1;
+        int r = nums.length - 1;
+        int ans = -1;
+
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                right_most = mid;
-                r = mid - 1;// left mae aur dekho 
-
-                //ye answer ho skta h par yahi answer nhi h 
-            } else if (nums[mid] < target) {
+            if (nums[mid] == target)
+            //ye answer ho skta h par yahi hoga we r not sure 
+            // so we travel to further right of the array 
+            {
+                ans = mid;
                 l = mid + 1;
 
-            } else {
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else
                 r = mid - 1;
 
-            }
         }
-        return right_most;
-
-
+        return ans;
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int left_most = findLeftMost(nums, target, n);
-        int right_most = findRightMost(nums, target, n);
-        return new int[] { left_most, right_most };
+        int n=nums.length;
+        
+        int left_most=findLeftMost(nums,target);
+        int right_most=findRightMost(nums,target);
+        return new int []{left_most,right_most};
 
+
+        
     }
 }
