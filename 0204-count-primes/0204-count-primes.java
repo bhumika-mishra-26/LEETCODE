fde0ebe31dd68522a82e98799f6a78fc4eb9ad90
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-07-26 16:06:02 +0530

    Time: 104 ms (51.23%), Space: 51.1 MB (72.39%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,4 +1,11 @@
 class Solution {
+
+
+    // 1. Standard sieve of eratosthenes complexity =nlog(lon n)
+    // 2.first make a boolean array of size n+1
+    // 3.now mark all of them as true;starting from 2 to less than n 
+    // 4.now for each nums[i] as true mark all its multiples as false from 2 till sqrt n 
+    
     public int countPrimes(int n) {
         boolean [] primes=new boolean  [n+1];
         for(int i=2;i<n;i++)
