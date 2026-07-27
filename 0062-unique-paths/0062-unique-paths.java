9a99ee025859451935b6a6d4a6c2398512455009
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-07-27 21:16:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,18 +16,35 @@ class Solution {
     public int uniquePaths(int m, int n) {
       
     //   return   recursion(m-1,n-1);
-      int [] dp=new int [n];
+    // har ek cell par aane ke ways will be dp[i-1][j]+dp[i][j-1]
+    //kyuki har cell par aane ka taereeka use just upar aur pehle wahe ka sum hoga 
+    //par for that u need to fill first row and first column \
+      int [][] dp=new int [m][n];
 
-      Arrays.fill(dp,1);
+    // fill 0th row 
+    dp[0][0]=0;
+
+    for(int i=0;i<n;i++)
+    {
+        dp[0][i]=1;
+
+    }
+    //fill 0th column 
+      for(int i=0;i<m;i++)
+    {
+        dp[i][0]=1;
+        
+    }
       for(int i=1;i<m;i++)
       {
         for(int j=1;j<n;j++)
         {
-           dp[j]=dp[j]+dp[j-1];
+        dp[i][j]=dp[i-1][j]+dp[i][j-1];
+
 
         }
       }
-      return dp[n-1];
+      return dp[m-1][n-1];
 
 
 
