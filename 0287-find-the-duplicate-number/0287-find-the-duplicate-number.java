eb9b8683d8b84da8ffc4e869b5eee5b9354e7f49
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-18 16:20:10 +0530

    Time: 4 ms (91.84%), Space: 82.6 MB (78.53%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    // har visited element par negative sign  
+    public int findDuplicate(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            int index=Math.abs(nums[i]);
+          
+            if(nums[index]<0)
+            return index;
+            nums[index]=-nums[index];
+
+
+        }
+        return -1;
+
+        
+    }
+}
