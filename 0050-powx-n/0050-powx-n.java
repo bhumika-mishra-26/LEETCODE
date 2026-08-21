2a894aeede414ce23f7e13678cdf7d56e3dfb9b6
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-21 22:28:30 +0530

    Time: 0 ms (100%), Space: 47.5 MB (85.48%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,25 +1,23 @@
 class Solution {
-    public double solve(double x,long n)
+    public double   solve(double x,long n)
     {
+        // so n does not go out of bound 
+        //time complexity is log n bcz we are dividing by two evry time 
         if(n==0)
         return 1;
         if(n<0)
-      return  solve(1/x,-n);
-
-        if(n%2==0)
-      return   solve(x*x,n/2);
-        else
-      return   x*solve(x*x,(n-1)/2);
+      return   solve(1/x,-n);
+        // so that if n is 214.... 2 raised to the power 31 it does not go out of boud when we convert it to integer
+         // if n is even 
+         if(n%2==0)
+          return solve(x*x,n/2);
+         else 
+        return  x*solve(x*x,(n-1)/2);
 
     }
     public double myPow(double x, int n) {
-        // n is odd 
-        // n*pow(n*n,n-1/2);
-        //n is even 
-        // pow(x*x,x/2)
-    return     solve(x,(long)n);
+   return      solve( x,(long)n);
 
-     
 
         
     }
