1d38c11812f709abe40b02ef808c7d11c0232253
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-22 12:24:05 +0530

    Time: 6 ms (87.72%), Space: 44.2 MB (47.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,50 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
+    public boolean matches (int [] count1,int [] count2 )
+    {
+        for(int i=0;i<26;i++)
+        {
+            if(count1[i]!=count2[i])
+            return false;
 
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
-
-        // Count the frequency of characters in s1 and the first window of s2
-        for (int i = 0; i < s1.length(); i++) {
-            s1Count[s1.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i) - 'a']++;
-        }
-
-        // Slide the window over s2
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1Count, s2Count)) return true;
-            // Update the window
-            s2Count[s2.charAt(i) - 'a']--;
-            s2Count[s2.charAt(i + s1.length()) - 'a']++;
-        }
-
-        // Check the last window
-        return matches(s1Count, s2Count);
-    }
-
-    private boolean matches(int[] s1Count, int[] s2Count) {
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] != s2Count[i]) return false;
         }
         return true;
+
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int n1=s1.length();
+        int n2=s2.length();
+        if(n1>n2)
+        return false;
+
+       
+        int [] s1_count=new int [26];
+        int [] s2_count=new int [26];
+        Arrays.fill(s1_count,0);
+        Arrays.fill(s2_count,0);
+
+        for(int i=0;i<n1;i++)
+        {
+            s1_count[s1.charAt(i)-'a']++;
+            s2_count[s2.charAt(i)-'a']++;
+
+        }
+  
+    if(matches(s1_count,s2_count))
+    return true;
+    
+    
+    for(int i=n1;i<n2;i++)
+    {
+      s2_count[s2.charAt(i)-'a']++;
+      s2_count[s2.charAt(i-n1)-'a']--;
+      if(matches(s1_count,s2_count))
+      return true;
+
+
+
+    }
+        return false;
+
+        
     }
 }
