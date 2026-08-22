9c3f1e0d690e7bb92d6520c83ffedbca5c740ccc
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-22 14:42:59 +0530

    Time: 325 ms (30.08%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,13 +3,25 @@
 -- FROM EMPLOYEE
 -- WHERE SALARY <(SELECT MAX(SALARY) FROM EMPLOYEE);
 
-SELECT MAX(SALARY) AS SecondHighestSalary
- FROM(
-    SELECT SALARY ,
-    DENSE_RANK() OVER (ORDER BY SALARY DESC) AS RNK 
-    FROM EMPLOYEE
- ) T
- WHERE RNK=2;
+
+-- remember max over null set return null so we add max(salary)
+-- SELECT MAX(SALARY) AS SecondHighestSalary
+--  FROM(
+--     SELECT SALARY ,
+--     DENSE_RANK() OVER (ORDER BY SALARY DESC) AS RNK 
+--     FROM EMPLOYEE
+--  ) T
+--  WHERE RNK=2;
+
+SELECT (
+    SELECT DISTINCT SALARY from employee
+    order by salary desc 
+    limit 1 offset 1
+
+)
+AS SecondHighestSalary;
+
+
 
 
 
