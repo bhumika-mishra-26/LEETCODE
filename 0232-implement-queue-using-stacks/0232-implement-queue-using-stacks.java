fc1ca8e3ca1c3a9839efa08983979d72338acbf3
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-24 12:28:45 +0530

    Time: 0 ms (100%), Space: 43 MB (15.36%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,6 +1,10 @@
 class MyQueue {
-    Stack<Integer>input=new Stack<>();
-     Stack<Integer>output=new Stack<>();
+
+
+// isme what we do is we create two stacks for queue implementation one will be the input stack and the other will be the output stack so what we do is we put new eleemnts in the input stack but jab pop and poll ki baari aaegi we can just pop elemts from the output stack isf it is not empty if empty saare elemts nikaal lo input se and output mai daal do 
+
+Stack<Integer>input=new Stack<>();
+Stack<Integer>output=new Stack<>();
 
 
     public MyQueue() {
@@ -11,47 +15,58 @@ class MyQueue {
         input.push(x);
 
         
+        
     }
     
     public int pop() {
-        if(output.isEmpty())
+        if(!output.isEmpty())
+        {
+            return output.pop();
+
+
+
+        }
+        else 
         {
             while(!input.isEmpty())
             {
                 output.push(input.pop());
 
+
             }
-        }
-       
             return output.pop();
 
-        
+        }
 
         
     }
     
     public int peek() {
-           if(output.isEmpty())
+        if(!output.isEmpty())
         {
+            return output.peek();
+
+        }
+        else{
             while(!input.isEmpty())
             {
                 output.push(input.pop());
 
+
             }
-        }
-       
             return output.peek();
-        
+            
+
+        }
     }
     
     public boolean empty() {
-        if(input.isEmpty() && output.isEmpty())
-        {
-            return true;
+        if(output.size()==0 &&  input.size()==0)
+        return true;
+        else 
+        return false;
 
-        }
-        else return false;
-
+        
     }
 }
 
