1a712351d737bde33bbcc1e3f8e1302264a3707c
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-25 22:45:29 +0530

    Time: 84 ms (5.81%), Space: 106.6 MB (73.92%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,26 +3,30 @@ class Solution {
         int n=temperatures.length;
         int [] ans=new int [n];
         Arrays.fill(ans,0);
-
         Stack<Integer>st=new Stack<>();
-
         for(int i=n-1;i>=0;i--)
         {
-            while(!st.isEmpty() && temperatures[i]>=temperatures[st.peek()])
+            while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[i])
             {
                 st.pop();
 
             }
+           
+
+            
             if(st.isEmpty())
-            ans[i]=0;
+            {
+                ans[i]=0;
+
+            }
             else{
                 ans[i]=st.peek()-i;
 
             }
-
-           st.push(i);
+            st.push(i);
 
         }
         return ans;
+
     }
 }
