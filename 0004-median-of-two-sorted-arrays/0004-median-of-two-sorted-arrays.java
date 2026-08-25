5da3c7b8b176bb5b85d8531d7397c8eae77620eb
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-25 14:16:02 +0530

    Time: 2 ms (46.93%), Space: 49 MB (15.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,78 +1,97 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int size = n1 + n2 ;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int ele1 = -1;
-        int ele2 = -1;
-        int idx1 = size / 2 - 1;
-        int idx2 = size / 2;
-        while (i < n1 && j < n2) {
-            if (nums1[i] <= nums2[j]) {
-                if (k == idx1) {
-                    ele1 = nums1[i];
-                 
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int idx1=(n1+n2)/2-1;
+        int idx2=(n1+n2)/2;
+        int ele1=-1;
+        int ele2=-1;
+        int i=0;
+        int j=0;
+        int k=0;
+
+
+        while(i<n1 && j<n2)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+               
+                if(k==idx1)
+                {
+                    ele1=nums1[i];
+
                 }
-                if (k == idx2) {
-                    ele2 = nums1[i];
-                   
+                if(k==idx2)
+                {
+                    ele2=nums1[i];
+
+                }   
+                k++;
+                i++;
+
+                
+            }
+            else{
+             
+                  if(k==idx1)
+                {
+                    ele1=nums2[j];
+
                 }
+                if(k==idx2)
+                {
+                    ele2=nums2[j];
+
+                }   
+                k++;
+                j++;
+
+
+
+            }
+        }
+        while(i<n1)
+        {
+         
+
+             if(k==idx1)
+                {
+                    ele1=nums1[i];
+
+                }
+                if(k==idx2)
+                {
+                    ele2=nums1[i];
+
+                }   
                 i++;
                 k++;
 
-            }
-
-            else {
-                if (k == idx1) {
-                    ele1 = nums2[j];
-                  
-                }
-                if (k == idx2) {
-                    ele2 = nums2[j];
-                   
+                
+        }
+        while(j<n2)
+        {
+           
+             if(k==idx1)
+                {
+                    ele1=nums2[j];
 
                 }
+                if(k==idx2)
+                {
+                    ele2=nums2[j];
+
+                }   
                 j++;
                 k++;
-            }
 
+                
         }
-        while (i < n1) {
-            if (k == idx1) {
-                ele1 = nums1[i];
-               
-            }
-            if (k == idx2) {
-                ele2 = nums1[i];
-              
-            }
-            i++;
-            k++;
-
-        }
-        while (j < n2) {
-            if (k == idx1) {
-                ele1 = nums2[j];
-              
-
-            }
-            if (k == idx2) {
-                ele2 = nums2[j];
-             
-
-            }
-            j++;
-
-            k++;
-
-        }
-        if (size % 2 == 0)
-            return (ele1 + ele2) / 2.0;
+        if((n1+n2)%2!=0)
+        return ele2;
         else
-            return ele2;
+        return (ele1+ele2)/2.0;
 
+        
     }
 }
