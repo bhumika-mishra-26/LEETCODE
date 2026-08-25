41070760bac7d3f93a7b97cb1a12f75c5cff76bb
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-25 23:17:05 +0530

    Time: 4 ms (74.12%), Space: 45.3 MB (50.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,42 +1,42 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
+        int [] ans=new int [nums2.length];
+        Arrays.fill(ans,0);
+
         Stack<Integer>st=new Stack<>();
 
-
-        int [] greater=new int [n2];
-        for(int i=n2-1;i>=0;i--)
+        for(int i=nums2.length-1;i>=0;i--)
         {
-            while(!st.isEmpty() && st.peek()<=nums2[i])
+            while(!st.isEmpty() && st.peek()<nums2[i])
             {
                 st.pop();
 
             }
             if(st.isEmpty())
-            {
-                greater[i]=-1;
-
-            }
-            else{
-                greater[i]=st.peek();
-
-            }
+            ans[i]=-1;
+            else
+            ans[i]=st.peek();
             st.push(nums2[i]);
 
-        }
-HashMap<Integer,Integer>mp=new HashMap<>();
-for(int i=0;i<n2;i++)
-{
-    mp.put(nums2[i],greater[i]);
 
-}
-int [] ans=new int [n1];
-for(int i=0;i<n1;i++)
-{
-    ans[i]=mp.get(nums1[i]);
-}
-return ans;
-        
+
+
+
+        }
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<ans.length;i++)
+        {
+            mp.put(nums2[i],ans[i]);
+
+        }
+        int [] res=new int [nums1.length];
+        for(int i=0;i<res.length;i++)
+        {
+            
+                res[i]=mp.get(nums1[i]);
+            
+        }
+        return res;
+
     }
 }
