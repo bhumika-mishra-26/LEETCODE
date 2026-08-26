e6bea4e216b105e468d00ea748fcb1f8272b301c
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-26 12:14:46 +0530

    Time: 15 ms (60.21%), Space: 48.5 MB (48.73%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,28 +1,33 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        // sirf array ko double krna pretend kran  h rest is same as nge1 
         int n=nums.length;
         Stack<Integer>st=new Stack<>();
+
+
         int [] ans=new int [n];
-
-
         for(int i=2*n-1;i>=0;i--)
-
         {
             while(!st.isEmpty() && st.peek()<=nums[i%n])
             {
                 st.pop();
-
             }
             if(i<n)
             {
-                ans[i]=st.isEmpty()?-1:st.peek();
-
+            if(st.isEmpty())
+            ans[i]=-1;
+            else
+            ans[i]=st.peek();
             }
             st.push(nums[i%n]);
 
 
+
+
+
         }
         return ans;
+
         
     }
 }
