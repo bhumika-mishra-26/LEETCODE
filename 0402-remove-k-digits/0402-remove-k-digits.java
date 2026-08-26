77319f722eba59cd771e17487a1d90a069342b7a
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-26 18:55:34 +0530

    Time: 27 ms (29.58%), Space: 47.4 MB (59.23%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,4 +1,6 @@
 class Solution {
+    // isme dekho ek string h jaise ki 1234 to hulog hamesha small number banane ke liye 
+    // increasing order mai numbers ko place krte h aur hamesha msb wali digit ko pop krte h agar increasing order mai nhi h kyuki vhi digit badi hogi hamesha inc order seq follow hoga 
     public String removeKdigits(String num, int k) {
         Stack<Character>st=new Stack<>();
         for(char c:num.toCharArray())
@@ -6,33 +8,40 @@ class Solution {
             while(!st.isEmpty() && st.peek()>c && k>0)
             {
                 st.pop();
-                k--;
+                k-=1;
+
 
             }
             st.push(c);
-        }
 
-        while(!st.isEmpty() && k>0 )
+            
+        }
+        while(!st.isEmpty() && k>0)
         {
-            st.pop();
-            k--;
+           st.pop();
+           k-=1;
+           
 
 
         }
-StringBuilder res=new StringBuilder ();
-while(!st.isEmpty())
-{
-    res.append(st.pop());
-}
-res.reverse();
+        StringBuilder str=new StringBuilder();
+         while(!st.isEmpty() )
+        {
+           str.append(st.pop());
+           
+
+        }
+
+str.reverse();
 int id=0;
-while(id<res.length() && res.charAt(id)=='0')
+while(id<str.length() && str.charAt(id)=='0')
 {
     id++;
 
 }
-StringBuilder ans=new StringBuilder(res.substring(id));
-return ans.length()==0?"0":ans.toString();
+StringBuilder res=new StringBuilder(str.substring(id));
+return res.length()==0?"0":res.toString();
+
 
         
     }
