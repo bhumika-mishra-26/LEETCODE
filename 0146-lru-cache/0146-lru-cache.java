1664ecd6296aa3bc12c58ed0b2a262c4f26c957e
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-28 00:30:00 +0530

    Time: 14 ms (98.69%), Space: 58.8 MB (89.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -95,7 +95,8 @@
 //             return;
 //         }
 
-//         if (cache.size() >= capacity) {
+//        
+//  if (cache.size() >= capacity) {
 //             ListNode removedNode = cacheQueue.removeLast();
 //             cache.remove(removedNode.key);
 //         }
@@ -112,124 +113,125 @@
 
 
 // isme we need to get the element in 0(1) time so hasmap does that but it does not preserve the insetion order so we use the doubly linked list
+import java.util.*;
 
-class ListNode{
+class ListNode {
     int key;
     int value;
     ListNode prev;
     ListNode next;
-    ListNode(int key,int value)
-    {
-        this.key=key;
-        this.value=value;
 
+    ListNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class DoublyLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+
+    DoublyLinkedList() {
+        head = new ListNode(-1, -1);
+        tail = new ListNode(-1, -1);
+
+        head.next = tail;
+        tail.prev = head;
     }
 
+    void addFirst(ListNode node) {
+
+        ListNode front = head.next;
+
+        node.next = front;
+        node.prev = head;
+
+        head.next = node;
+        front.prev = node;
+    }
+
+    void remove(ListNode node) {
+
+        ListNode prevNode = node.prev;
+        ListNode nextNode = node.next;
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+
+        node.prev = null;
+        node.next = null;
+    }
+
+    ListNode removeLast() {
+
+        ListNode last = tail.prev;
+
+        if(last == head)
+            return null;
+
+        remove(last);
+
+        return last;
+    }
 }
-class DoublyLinkedList{
-ListNode head;
-ListNode tail;
-DoublyLinkedList()
-{
-     head=new ListNode(-1,-1);
-     tail=new ListNode (-1,-1);
-    head.next=tail;
-    tail.prev=head;
-
-}
-public void addFirst(ListNode node)
-{
-   
-    ListNode sec=head.next;
-    node.prev=head;
-    node.next=sec;
-    head.next=node;
-    sec.prev=node;
-
-
-}
-public void remove(ListNode node)
-{
-    ListNode prevNode =node.prev;
-    ListNode nextNode=node.next;
-    node.prev=null;
-    node.next=null;
-    prevNode.next=nextNode;
-    nextNode.prev=prevNode;
-
-}
-
-public ListNode  removeLast(  )
-{
-    ListNode sec=tail.prev;
-    if(sec==head)
-    return null;
-    remove(sec);
-    return sec;
-}
-
-}
-
 
 class LRUCache {
-    int capacity;
-    Map<Integer,ListNode>cache;
-    DoublyLinkedList cachedQueue;
 
-
+    private int capacity;
+    private HashMap<Integer, ListNode> cache;
+    private DoublyLinkedList cacheQueue;
 
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        this.cache=new HashMap<>();
-        this.cachedQueue=new DoublyLinkedList();
 
-
-        
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.cacheQueue = new DoublyLinkedList();
     }
-    
+
     public int get(int key) {
+
         if(!cache.containsKey(key))
-        {
             return -1;
 
-        }
-        ListNode getting=cache.get(key);
-    cachedQueue.remove(getting);
-    cachedQueue.addFirst(getting);
-    return getting.value;
+        ListNode node = cache.get(key);
 
+        // node ko current position se hatao
+        cacheQueue.remove(node);
 
+        // front = most recently used
+        cacheQueue.addFirst(node);
+
+        return node.value;
     }
-    
+
     public void put(int key, int value) {
-        if(cache.containsKey(key))
-        {
-ListNode new_node=cache.get(key);
-new_node.value=value;
-cachedQueue.remove(new_node);
-cachedQueue.addFirst(new_node);
-return ;
 
+        // key already present
+        if(cache.containsKey(key)) {
 
+            ListNode node = cache.get(key);
+
+            node.value = value;
+
+            cacheQueue.remove(node);
+            cacheQueue.addFirst(node);
+
+            return;
         }
-        if(cache.size()>=capacity)
-        {
-            ListNode sec=cachedQueue.removeLast();
-            cache.remove(sec.key);
 
+        // capacity full hai
+        if(cache.size() >= capacity) {
 
+            ListNode removedNode = cacheQueue.removeLast();
+
+            cache.remove(removedNode.key);
         }
+
+        // new node
         ListNode newNode = new ListNode(key, value);
-              cachedQueue.addFirst(newNode);
-              cache.put(key, newNode);
-  
 
+        cacheQueue.addFirst(newNode);
+        cache.put(key, newNode);
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
