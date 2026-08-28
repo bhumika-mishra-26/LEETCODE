f048f65abfdc600c6bb52e867376bac6abcf5303
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-08-29 00:17:04 +0530

    Time: 1 ms (30.71%), Space: 45.6 MB (59.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,32 +13,30 @@
  *     }
  * }
  */
-
-
-
 class Solution {
-    //  1. isme pehle ye find kro ki jo left aur right subtree hoga uski height kya hogi kyuki tabhi we can find whether a tree is balanced or not 
+    public int height(TreeNode root)
+    {
+        if(root==null)
+        return 0;
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return 1+Math.max(lh,rh);
 
-//  2.fir jab left height aur right height nikal jaye then find kro ki vo balanced h ya nhi 
-public int height(TreeNode root)
-{
-    if(root==null)
-    return 0;
-    int lh=height(root.left);
-    int rh=height(root.right);
-    return Math.max(lh,rh)+1;
-    
-
-}
+    }
     public boolean isBalanced(TreeNode root) {
         if(root==null)
         return true;
+
+      
+    //  1. isme pehle ye find kro ki jo left aur right subtree hoga uski height kya hogi kyuki tabhi we can find whether a tree is balanced or not 
+
+//  2.fir jab left height aur right height nikal jaye then find kro ki vo balanced h ya nhi
         int lh=height(root.left);
         int rh=height(root.right);
         if(Math.abs(lh-rh)>1)
         return false;
-        //matlab iske aage we need not check for any tree 
-        return isBalanced(root.left) && isBalanced(root.right);
+
+        return isBalanced(root.left)&& isBalanced(root.right);
 
     }
 }
