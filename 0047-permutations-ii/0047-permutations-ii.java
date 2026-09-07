0125985d89ab4e8e523a3032fae39b28ce70ee7d
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-09-08 00:04:55 +0530

    Time: 2 ms (99.04%), Space: 47.2 MB (5.78%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,58 +1,50 @@
 class Solution {
-  
-    public void swap(int [] nums,int first,int sec)
-    {
-        int temp=nums[first];
-        nums[first]=nums[sec];
-        nums[sec]=temp;
 
+    public void swap(int[] nums, int f, int s) {
+        int temp = nums[f];
+        nums[f] = nums[s];
+        nums[s] = temp;
     }
-    public void solve(int idx,int n,List<List<Integer>>ans,int [] nums)
-    {//
-    // logic ye hoga ki uss index har har value aa jaani chahiye jo nhi aai h 
-    
-        if(idx==n)
-        {
-       List<Integer>arr=new ArrayList<>();
-       for(int num:nums)
-       {
-        arr.add(num);
 
-       }
-       ans.add(arr);
-       return ;
+    public void solve(int idx, int[] nums, int n, List<List<Integer>> ans) {
+
+        if(idx == n) {
+            ArrayList<Integer> res = new ArrayList<>();
+
+            for(int i : nums) {
+                res.add(i);
+            }
+
+            ans.add(res);
+            return;
         }
-        HashSet<Integer>st=new HashSet<>();
 
+        HashSet<Integer> st = new HashSet<>();
 
-        for(int i=idx;i<nums.length;i++)
-        {//
-        if(st.contains(nums[i]))
-        continue;
-         
+        for(int i = idx; i < nums.length; i++) {
+
+            // Same value ko same level par dobara choose nahi karna
+            if(st.contains(nums[i]))
+                continue;
 
             st.add(nums[i]);
-   //     isme swap karenge  karenge 
-            swap(nums,i,idx);
-            //explore step
-            solve(idx+1,n,ans,nums);
-            //undo this step
-            swap(nums,i,idx);
 
-        
+            swap(nums, i, idx);
 
+            solve(idx + 1, nums, n, ans);
+
+            swap(nums, i, idx);
         }
-
-
     }
-      public List<List<Integer>> permuteUnique(int[] nums) {
-         int n=nums.length;
-        int idx=0;
-        List<List<Integer>>ans=new ArrayList<>();
 
-        solve(idx,n,ans,nums);
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int n = nums.length;
+
+        solve(0, nums, n, ans);
+
         return ans;
     }
 }
- 
-  
