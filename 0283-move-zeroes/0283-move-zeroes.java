df7c50d743b235b841a0d3b98dcc9d335b40de43
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-09-20 18:15:25 +0530

    Time: 2 ms (91.67%), Space: 48 MB (6.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,23 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int nonZero=0;
+     int nonZero=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(nums[i]!=0)
+        nums[nonZero++]=nums[i];
 
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=0)
-            {
-                nums[nonZero++]=nums[i];
+     }
+     for(int i=nonZero;i<nums.length;i++)
+     {
+        nums[i]=0;
 
+     }
 
-            }
-        }
-        for(int i=nonZero;i<n;i++)
-        {
-            nums[i]=0;
-
-        }
-    
 
     }
 }
