a9af1360e173334192429f2f6ee0e45b4278089d
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-09-20 17:32:07 +0530

    Time: 0 ms (100%), Space: 43.2 MB (16.21%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+     s=s.trim();
+     int count=0;
+
+     for(int i=s.length()-1;i>=0;i--)
+     {
+        if(s.charAt(i)!=' ')
+        {
+            count+=1;
+
+        }
+       else if(count>0)
+        break;
+        
+     }
+     return count;
+
+
+    }
+}
